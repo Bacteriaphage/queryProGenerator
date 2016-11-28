72296682f690925acced483bafc6456b6bc64509
Bacteriaphage <tohyzhang@hotmail.com>
2016-11-28 16:25:09 -0500

    1.0

--- src/queryProGenerator/queryProGenerator.java
+++ src/queryProGenerator/queryProGenerator.java
@@ -50,10 +50,11 @@ public class queryProGenerator {
 			try{
 				file.createNewFile();
 				FileWriter fileWriter = new FileWriter(file);
-				outputFrame(fileWriter);
+				outputFrame(fileWriter, myQuery);
 				outputMFstruct(MFstructure, fileWriter);
+				outputProcessFunc(fileWriter, myQuery, MFstructure);
+				outputOutputFunc(fileWriter, myQuery, MFstructure);
 				outputMainFunc(fileWriter);
-				outputProcessFunc(fileWriter, myQuery);
 				fileWriter.close();
 			}catch(IOException e){
 				e.printStackTrace();
@@ -71,24 +72,167 @@ public class queryProGenerator {
 			exception.printStackTrace();
 		}
 	}
-	static void outputProcessFunc(FileWriter fileWriter, Query myQuery){
+	static void outputOutputFunc(FileWriter fileWriter, Query myQuery, HashMap<String, String> MFstructure){
+		String output = new String("void output(struct Data *data, int i){\n");
+		output += "\tprintf(\"";
+		for(String obj : myQuery.select){
+			String[] temp = obj.split("_");
+			if(temp[temp.length-1].equals("cust")){
+				output += " CUSTOMER ";
+			}
+			else if(temp[temp.length-1].equals("prod")){
+				output += " PRODUCT ";
+			}
+			else if(temp[temp.length-1].equals("quant")){
+				output += " " + obj + " ";
+			}
+			else if(temp[temp.length-1].equals("state")){
+				output += " STATE ";
+			}
+			else if(temp[temp.length-1].equals("day")){
+				output += " DAY ";
+			}
+			else if(temp[temp.length-1].equals("month")){
+				output += " MONTH ";
+			}
+			else if(temp[temp.length-1].equals("year")){
+				output += " YEAR ";
+			}
+			else{
+				output += " " + obj + " ";
+			}
+ 		}
+		output += "\\n\");\n";
+		output += "\tprintf(\"" ;
+		for(String obj : myQuery.select){
+			String[] temp = obj.split("_");
+			if(temp[temp.length-1].equals("cust")){
+				output += " ======== ";
+			}
+			else if(temp[temp.length-1].equals("prod")){
+				output += " ======= ";
+			}
+			else if(temp[temp.length-1].equals("quant")){
+				output += " ";
+				for(int i = 0; i < obj.length(); i++){
+					output += "=";
+				}
+				output += " ";
+			}
+			else if(temp[temp.length-1].equals("state")){
+				output += " ===== ";
+			}
+			else if(temp[temp.length-1].equals("day")){
+				output += " === ";
+			}
+			else if(temp[temp.length-1].equals("month")){
+				output += " ===== ";
+			}
+			else if(temp[temp.length-1].equals("year")){
+				output += " ==== ";
+			}
+			else{
+				output += " ";
+				for(int i = 0; i < obj.length(); i++){
+					output += "=";
+				}
+				output += " ";
+			}
+ 		}
+		output += "\\n\");\n";
+		output += "\tfor(int j = 0; j < i; j++){\n";
+		if(myQuery.havingCondi.size() != 0){
+			output += "\t\tif(";
+			for(String obj:myQuery.havingCondi){
+				if(!obj.equals("and") && !obj.equals("or")){
+					String first = new String();
+					String operator = new String();
+					String second = new String();
+					boolean flag = true;
+					for(int i = 0; i < obj.length(); i++){
+						if(obj.charAt(i) >= 'a' && obj.charAt(i) <= 'z' || obj.charAt(i) >= 'A' && obj.charAt(i) <= 'Z' ||
+								obj.charAt(i) >= '0' && obj.charAt(i) <= '9' || obj.charAt(i) == '_'){
+							if(flag){
+								first += obj.charAt(i);
+							}
+							else{
+								second += obj.charAt(i);
+							}
+						}
+						else{
+							operator += obj.charAt(i);
+							flag = false;
+						}
+					}
+					output += "data[j]." + first + (operator.equals("=") ? "==" : operator) + second;
+				}
+				else if(obj.equals("and")){
+					output += "&&";
+				}
+				else if(obj.equals("or")){
+					output += "or";
+				}
+			}
+			output += "){\n";
+		}
+		for(String obj : myQuery.select){
+			output += "\t\t\tprintf(\"";
+			String[] temp = obj.split("_");
+			if(temp[temp.length-1].equals("cust")){
+				output += " %-8s \",data[j]." + obj + ");\n";
+			}
+			else if(temp[temp.length-1].equals("prod")){
+				output += " %-7s \",data[j]." + obj + ");\n";
+			}
+			else if(temp[temp.length-1].equals("quant")){
+				output += " %" + Integer.toString(obj.length())+"d \",data[j]." + obj + ");\n";
+			}
+			else if(temp[temp.length-1].equals("state")){
+				output += " %-2s \",data[j]." + obj + ");\n";
+			}
+			else if(temp[temp.length-1].equals("day")){
+				output += " %02d \",data[j]." + obj + ");\n";
+			}
+			else if(temp[temp.length-1].equals("month")){
+				output += " %02d \",data[j]." + obj + ");\n";
+			}
+			else if(temp[temp.length-1].equals("year")){
+				output += " %02d \",data[j]." + obj + ");\n";
+			}
+			else{
+				output += " %" + Integer.toString(obj.length())+"d \",data[j]." + obj + ");\n";
+			}
+		}
+		if(myQuery.havingCondi.size() != 0){
+			output += "\t\t}\n";
+		}
+		output += "\t}\n";
+		output += "}\n";
+		try{
+			fileWriter.write(output);
+			System.out.println("finish to output!");
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+	}
+	static void outputProcessFunc(FileWriter fileWriter, Query myQuery, HashMap<String, String> MFstructure){
 		String output = new String("void process(struct Data *data, int *i){\n");
 		int aggreFuncIndex = 0;
 		int suchThatIndex = 0;
-		int havingIndex = 0;
-		for(int NGV = 0; NGV <= myQuery.numOfGV; NGV++){
+		for(int NGV = 0; NGV <= myQuery.numOfGV; NGV++){                                    //count grouping variables
 			if(NGV == 0){
-				String[] temp = myQuery.aggreFunc.get(0).split("_");
+				String[] temp = myQuery.aggreFunc.get(0).split("_");                        //look for aggregate func for GV0
 				if(temp[1] != "0") continue; 
 			}
 			output += "\tEXEC SQL DECLARE mycursor CURSOR FOR SELECT * FROM sales;\n";
 			output += "\tEXEC SQL SET TRANSACTION read only;\n";
 			output += "\tEXEC SQL OPEN mycursor;\n";
-			output += "\tEXEC SQL FETCH FROM mycursor INTO :sale_rec;\n";
+			output += "\tEXEC SQL FETCH FROM mycursor INTO :sale_rec"+Integer.toString(NGV)+";\n";
 			output += "\twhile(sqlca.sqlcode == 0){\n";
 			boolean condi = true;
 			if(NGV == 0){
-				String[] temp = myQuery.aggreFunc.get(aggreFuncIndex).split("_");
+				String[] temp = myQuery.aggreFunc.get(aggreFuncIndex).split("_");           //find such that condition.
 				if(temp[1].equals("0")){
 					condi = false;
 				}
@@ -97,15 +241,19 @@ public class queryProGenerator {
 			output += "\t\t"+"for(j = 0; j < *i; j++)\n";
 			output += "\t\t\t"+"if(";
 			for(String obj : myQuery.groupingAttri){
-				output += "strcmp(data[j]."+obj+", sale_rec."+obj+")==0&&";
+				if(MFstructure.get(obj).equals("character varying") || MFstructure.get(obj).equals("character"))
+					output += "strcmp(data[j]."+obj+", sale_rec"+Integer.toString(NGV)+"."+obj+")==0&&";
+				else if(MFstructure.get(obj).equals("Integer"))
+					output += "data[j]." + obj + "== sale_rec"+Integer.toString(NGV)+"." + obj +"&&";
 			}
 			output = output.substring(0, output.length()- 2);
 			output += ") break;\n";
-			output += "\t\t\t" + "if(j == *i){\n";
+			output += "\t\t" + "if(j == *i){\n";
 			if(condi){
-				output += "\t\t\t\tif(";
+				output += "\t\t\tif(";
+				int i = suchThatIndex;
 				while(true){
-					String[] temp = myQuery.suchThat.get(suchThatIndex).split("_");
+					String[] temp = myQuery.suchThat.get(i).split("_");
 					System.out.println(temp[0]);
 					if(!(temp[0].equals(Integer.toString(NGV)))) break;
 					String GVattri = new String();
@@ -124,18 +272,107 @@ public class queryProGenerator {
 						operator += temp[1].charAt(sign);
 						sign++;
 					}
+					if(operator.equals("=")){
+						operator += "=";
+					}
 					for(;sign < temp[1].length(); sign++){
 						OBJattri += temp[1].charAt(sign);
 					}
 					if(temp.length == 4){
 						OBJattri += "_" + temp[2] + "_" + temp[3];
-						output += "sale_rec." + GVattri + operator + "data[j]."+OBJattri;
+						output += "sale_rec"+Integer.toString(NGV)+"." + GVattri + operator + "data[j]."+OBJattri;
 					}
 					else{
-						output += "sale_rec." + GVattri + operator + OBJattri;
+						output += "sale_rec"+Integer.toString(NGV)+"." + GVattri + operator + OBJattri;
 					}
+					if(i + 1 >= myQuery.suchThat.size()) break;
+					String next = myQuery.suchThat.get(i + 1);
+					if(next.equals("and") || next.equals("or")){
+						if(next.equals("and")){
+							output += " && ";
+						}
+						else{
+							output += " || ";
+						}
+						i += 2;   //get next condition;
+					}
+					else{
+						break;
+					}
+				}
+				output += "){\n";
+			}
+			for(String obj : myQuery.groupingAttri){
+				if(MFstructure.get(obj).equals("character varying") || MFstructure.get(obj).equals("character"))
+					output += "\t\t\t\tstrcpy(data[*i]." + obj + ", "+ "sale_rec"+Integer.toString(NGV)+"." + obj +");\n";
+				else if(MFstructure.get(obj).equals("Integer"))
+					output += "\t\t\t\tdata[j]." + obj + "= sale_rec"+Integer.toString(NGV)+"." + obj +";\n";
+			}
+			for(int i = aggreFuncIndex ;i < myQuery.aggreFunc.size(); i++){
+				String[] temp = myQuery.aggreFunc.get(i).split("_");
+				if(!Integer.toString(NGV).equals(temp[1])) break;
+				else{
+					if(temp[0].equals("sum")){
+						output += "\t\t\t\tdata[j]." + myQuery.aggreFunc.get(i) + "=sale_rec"+Integer.toString(NGV)+"." + temp[2] + ";\n" ;
+					}
+					else if(temp[0].equals("count")){
+						output += "\t\t\t\tdata[j]." + myQuery.aggreFunc.get(i) + "=1" +";\n" ;
+					}
+					else if(temp[0].equals("min")){
+						output += "\t\t\t\tdata[j]." + myQuery.aggreFunc.get(i) + "=sale_rec"+Integer.toString(NGV)+"." + temp[2] + ";\n";
+					}
+					else if(temp[0].equals("max")){
+						output += "\t\t\t\tdata[j]." + myQuery.aggreFunc.get(i) + "=sale_rec"+Integer.toString(NGV)+"." + temp[2] + ";\n";
+					}
+					else if(temp[0].equals("avg")){
+						output += "\t\t\t\tdata[j]." + myQuery.aggreFunc.get(i) + "=data[j].sum/data[j].count;\n";
+					}
+					else{
+						System.out.println("unknow aggregate func");
+					}
+				}
+			}
+			output += "\t\t\t\t(*i)++;\n";
+			if(condi)
+				output += "\t\t\t}\n";
+			output += "\t\t}\n";
+			output += "\t\telse{\n";
+			if(condi){
+				output += "\t\t\tif(";
+				while(true){
+					String[] temp = myQuery.suchThat.get(suchThatIndex).split("_");
+					if(!(temp[0].equals(Integer.toString(NGV)))) break;
+					String GVattri = new String();
+					String OBJattri = new String();
+					String operator = new String();
+					int sign = 0;
+					for(;temp[1].charAt(sign)>='a' && temp[1].charAt(sign)<='z' || 
+							temp[1].charAt(sign)>='A' && temp[1].charAt(sign)<='Z' || 
+							temp[1].charAt(sign)>='0' && temp[1].charAt(sign)<='9'; 
+							sign++){
+						GVattri += temp[1].charAt(sign);
+					}
+					while(!(temp[1].charAt(sign)>='a' && temp[1].charAt(sign)<='z' || 
+							temp[1].charAt(sign)>='A' && temp[1].charAt(sign)<='Z' ||
+							temp[1].charAt(sign)>='0' && temp[1].charAt(sign)<='9')){
+						operator += temp[1].charAt(sign);
+						sign++;
+					}
+					if(operator.equals("=")){
+						operator += "=";
+					}
+					for(;sign < temp[1].length(); sign++){
+						OBJattri += temp[1].charAt(sign);
+					}
+					if(temp.length == 4){
+						OBJattri += "_" + temp[2] + "_" + temp[3];
+						output += "sale_rec"+Integer.toString(NGV)+"." + GVattri + operator + "data[j]."+OBJattri;
+					}
+					else{
+						output += "sale_rec"+Integer.toString(NGV)+"." + GVattri + operator + OBJattri;
+					}
+					if(suchThatIndex + 1 >= myQuery.suchThat.size()) break;
 					String next = myQuery.suchThat.get(suchThatIndex + 1);
-					
 					if(next.equals("and") || next.equals("or")){
 						if(next.equals("and")){
 							output += " && ";
@@ -151,11 +388,39 @@ public class queryProGenerator {
 				}
 				output += "){\n";
 			}
-			for(String obj : myQuery.groupingAttri){
-				output += "\t\t\t\tstrcpy(data[*i]." + obj + ", "+ "sale_rec." + obj +");\n";
+			for(;aggreFuncIndex < myQuery.aggreFunc.size(); aggreFuncIndex++){
+				String[] temp = myQuery.aggreFunc.get(aggreFuncIndex).split("_");
+				if(!Integer.toString(NGV).equals(temp[1])) break;
+				else{
+					if(temp[0].equals("sum")){
+						output += "\t\t\t\tdata[j]." + myQuery.aggreFunc.get(aggreFuncIndex) + "+=sale_rec"+Integer.toString(NGV)+"." + temp[2] + ";\n" ;
+					}
+					else if(temp[0].equals("count")){
+						output += "\t\t\t\tdata[j]." + myQuery.aggreFunc.get(aggreFuncIndex) + "++" +";\n" ;
+					}
+					else if(temp[0].equals("min")){
+						output += "\t\t\t\tif(data[j]." + myQuery.aggreFunc.get(aggreFuncIndex) + ">sale_rec"+Integer.toString(NGV)+"." + temp[2] + ")\n";
+						output += "\t\t\t\t\tdata[j]." + myQuery.aggreFunc.get(aggreFuncIndex) + "=sale_rec"+Integer.toString(NGV)+"." + temp[2] + ";\n";
+					}
+					else if(temp[0].equals("max")){
+						output += "\t\t\t\tif(data[j]." + myQuery.aggreFunc.get(aggreFuncIndex) + "<sale_rec"+Integer.toString(NGV)+"." + temp[2] + ")\n";
+						output += "\t\t\t\t\tdata[j]." + myQuery.aggreFunc.get(aggreFuncIndex) + "=sale_rec"+Integer.toString(NGV)+"." + temp[2] + ";\n";
+					}
+					else if(temp[0].equals("avg")){
+						output += "\t\t\t\tdata[j]." + myQuery.aggreFunc.get(aggreFuncIndex) + "=data[j].sum/data[j].count;\n";
+					}
+					else{
+						System.out.println("unknow aggregate func");
+					}
+				}
+				if(condi)
+					output += "\t\t\t}\n";
+				output += "\t\t}\n";
+				output += "\t\tEXEC SQL FETCH FROM cursor INTO :sale_rec"+Integer.toString(NGV)+";\n";
+				output += "\t}\n";
+				output += "\tEXEC SQL CLOSE cursor;\n";
 			}
-			if(condi)
-				output += "\t\t\t\t}\n";
+			output += "}\n";
 			
 		}
 		try{
@@ -184,10 +449,14 @@ public class queryProGenerator {
 			e.printStackTrace();
 		}
 	}
-	static void outputFrame(FileWriter fileWriter) throws IOException{
+	static void outputFrame(FileWriter fileWriter, Query myQuery) throws IOException{
 		String output = new String("#include<stdio.h>\n#include<string.h>\n\n");
 		output += "EXEC SQL BEGIN DECLARE SECTION;\n";
-		output += "struct{\nchar\t*cust;\nchar\t*prod;\nshort\tdd;\nshort\tmm;\nshort\tyy;\nchar\t*state;\nlong\tquant;\n} sale_rec;\n";
+		output += "struct{\nchar\t*cust;\nchar\t*prod;\nshort\tdd;\nshort\tmm;\nshort\tyy;\nchar\t*state;\nlong\tquant;\n} sale_rec1";
+		for(int NGV = 2; NGV <= myQuery.numOfGV; NGV++){
+			output += ", sale_rec" + Integer.toString(NGV);
+		}
+		output += ";\n";
 		output += "EXEC SQL END DECLARE SECTION;\n";
 		output += "EXEC SQL INCLUDE sqlca;\n\n";
 		try{
